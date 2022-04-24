76b6f403a49294d2e81b1f9993aba8d56df1f705
DmitryKisel85 <dim03@mail.ru>
2022-04-24 15:41:09 +0300

    'updated_desktop_errors'

--- src/js/files/forms/inputmask.js
+++ src/js/files/forms/inputmask.js
@@ -7,7 +7,15 @@ import { flsModules } from "../modules.js";
 // Подключение модуля
 import "inputmask/dist/inputmask.min.js";
 
-const inputMasks = document.querySelectorAll('input');
+const inputMasks = document.querySelectorAll("input");
 if (inputMasks.length) {
 	flsModules.inputmask = Inputmask().mask(inputMasks);
 }
+
+const inputPhone = document.getElementById("phone");
+const phoneMask = new Inputmask("+ 7 ( 9 9 9 ) - 9 9 9 - 9 9 - 9 9");
+phoneMask.mask(inputPhone);
+
+const inputMail = document.getElementById("email");
+const mailMask = new Inputmask({ alias: "email" });
+mailMask.mask(inputMail);
